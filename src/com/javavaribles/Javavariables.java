986071592e56f174b8f885bda4f69baf1190cca4
Jyoti Sakhare <Jyoti Sakhare@DESKTOP-T6M756F>
2021-04-27 13:15:43 +0530

    Java Training Day first code committed

---
+++ src/com/javavaribles/Javavariables.java
@@ -0,0 +1,25 @@
+package com.javavaribles;
+
+public class Javavariables {
+	 //Global Variable
+	  int intEmpno =101;
+	  String strName = "Amit";
+	  int intSal = 5000;
+	  
+	  public void display(){
+		  System.out.println("Employee No :"+intEmpno + " The name is :"+strName);
+	  }
+	  
+	  public void totalsalary(){
+		//Local variable  
+		  
+	  }
+	  
+
+	public static void main(String[] args) {
+		Javavariables objJavavariables = new Javavariables();
+		objJavavariables.display();
+
+	}
+
+}
